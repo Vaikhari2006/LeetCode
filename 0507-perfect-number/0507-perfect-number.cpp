b8aba40048a16bcc513968e97163e2cfd8633f63
Vaikhari2006 <vaikharimatte@gmail.com>
2026-08-08 23:12:22 +0530

    Time: 0 ms (100%), Space: 7.8 MB (56.14%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -4,9 +4,11 @@ public:
         if(num<=0) return false;
         int sum=0;
 
-        for(int i=1;i<num;i++){
+        for(int i=1;i<sqrt(num);i++){
             if(num%i==0){
                 sum+=i;
+                int t=num/i;
+                if(t!=i && t!=num) sum+=t;
             }
         }
         return sum==num;
